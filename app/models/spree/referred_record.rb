207bf1f5277076fdeced199ace418e2b8510e1f1
Michael Davidson <mdavo6@gmail.com>
2021-04-21 16:40:02 +1000

    Update class_name for Rails 5.2 compatibility

--- app/models/spree/referred_record.rb
+++ app/models/spree/referred_record.rb
@@ -1,8 +1,8 @@
 module Spree
   class ReferredRecord < Spree::Base
     belongs_to :referral
-    belongs_to :user, class_name: Spree.user_class.to_s
-    belongs_to :order, class_name: Spree::Order
+    belongs_to :user, class_name: 'Spree::User'
+    belongs_to :order, class_name: 'Spree::Order'
     belongs_to :affiliate
     belongs_to :store_credit
 
